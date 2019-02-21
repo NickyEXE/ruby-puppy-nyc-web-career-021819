b5f6ec461b026cb7ff7fc1549941435f6828c9f2
Nicholas Dover <dovern42@gmail.com>
2019-02-21 05:56:13 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -8,17 +8,9 @@ class Dog
     @@all.push(self)
   end
   def self.all
-    binding.pry
     @@all.each do |obj|
       puts obj.dog
     end
-    binding.pry
   end
 end
-
-Dog.new("fido")
-Dog.new("chuck")
-Dog.new("larry")
-
-Dog.all
 
